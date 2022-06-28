3120e7cc6dfc278d39ad27f4e3357f0e209e6dde
Dest-01 <cesarmatarrita484@gmail.com>
2022-06-28 13:17:25 -0600

    changes ultim time

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,4 +10,4 @@ app.post('/api/task', TaskController.store)
 app.put('/api/task/:id', TaskController.update)
 app.delete('/api/task/:id', TaskController.destroy)
 
-module.exports = app
+module.exports = app    
